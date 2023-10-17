054d526d0519d9ac7f768d9e2a91fd8877815956
tong <Tong>
2023-10-17 11:52:48 +0800

    new file starter-vpn-proxy.sh

---
+++ Ubuntu/starter-vpn-proxy.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+## starter-vpn-proxy.sh
+
+# 定义函数来处理参数并打印结果
+vpn_start() {
+    # read -p "请输入VPN授权码: " vpn_auth_code
+    echo "\$1: " $1
+    vpn_auth_code="$1"
+    echo "vpn_auth_code: " $vpn_auth_code
+
+    while [[ ! $vpn_auth_code ]]
+    do
+        read -p "请输入VPN授权码: " vpn_auth_code
+        echo $vpn_auth_code
+    done
+
+    echo "STARTING VPN..."
+    sudo sed -i 's/: XAUTH.*/: XAUTH  '"$vpn_auth_code"'/g' /etc/ipsec.secrets
+    # sudo ipsec start --nofork | grep --color=auto authentication
+    # sudo ipsec start
+    sudo systemctl start strongswan-starter.service
+    ## 手工指定 DNS 解析服务器
+    # sudo sed -i '1i\nameserver 172.16.9.3' /etc/resolv.conf
+}
+
+vpn_status() {
+    echo "STATUS of VPN..."
+    # sudo ipsec status
+    systemctl status strongswan-starter.service
+}
+
+vpn_stop() {
+    echo "STOPPING VPN..."
+    # sudo ipsec stop
+    sudo systemctl stop strongswan-starter.service
+}
+
+proxy_start() {
+    echo "STARTING PROXY..."
+    sudo sed -i "s/^external:.*/external: $(hostname -I | awk '{print$2}')/" /etc/danted.conf
+    sudo systemctl restart danted.service
+}
+
+proxy_status() {
+    echo "STATUS of PROXY..."
+    systemctl status danted.service
+    # DATE=`date +"%b %d"` && awk '/$DATA/ && /dante/ {print}' /var/log/syslog
+}
+
+proxy_stop() {
+    echo "STOPPING PROXY..."
+    sudo systemctl stop danted.service
+}
+
+
+read -p "请输入要执行的操作: start(1), status(2), stop(3): " action
+
+case $action in
+    start|1)
+        vpn_start $1;
+        sleep 5
+        proxy_start;
+        ;;
+    status|2)
+        vpn_status;
+        echo -e "\n"
+        proxy_status;
+        ;;
+    stop|3)
+        vpn_stop;
+        proxy_stop;
+        ;;
+    *)
+        echo "你输入了无效操作! $action"
+        echo "Usage: $0 {start, status, stop or 1, 2, 3}."
+        ;;
+esac
