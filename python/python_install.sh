b80b9eb293cbd1806284babcba75c6e92615a4fa
TONG Leion <leion.tong@outlook.com>
2024-11-04 17:11:53 +0800

    update python_install.sh

--- python/python_install.sh
+++ python/python_install.sh
@@ -21,20 +21,21 @@ ln -svf $PREFIX/bin/python$MAJOR.$MINOR /usr/bin/python$MAJOR.$MINOR
 ln -svf $PREFIX/bin/pip$MAJOR.$MINOR /usr/bin/pip$MAJOR.$MINOR
 
 # 可选(创建 /usr/bin/python、/usr/bin/pip)
-mv /usr/bin/python{,.ori} || ln -svf python$MAJOR.$MINOR /usr/bin/python
-mv /usr/bin/pip{,.ori} || ln -svf pip$MAJOR.$MINOR /usr/bin/pip
+# mv /usr/bin/python{,.ori} || ln -svf python$MAJOR.$MINOR /usr/bin/python
+# mv /usr/bin/pip{,.ori} || ln -svf pip$MAJOR.$MINOR /usr/bin/pip
 
 # 可选(创建 /usr/bin/python3、/usr/bin/pip3)
 # 注意：会影响dnf、yum、yum-config-manager等命令: grep python `which dnf`.
-mv /usr/bin/python3{,.ori} || ln -svf python$MAJOR.$MINOR /usr/bin/python3
-mv /usr/bin/pip3{,.ori} || ln -svf pip$MAJOR.$MINOR /usr/bin/pip3
+# mv /usr/bin/python3{,.ori} || ln -svf python$MAJOR.$MINOR /usr/bin/python3
+# mv /usr/bin/pip3{,.ori} || ln -svf pip$MAJOR.$MINOR /usr/bin/pip3
 
 
-# 检查版本
-python --version
-pip --version
-python3 --version
-pip3 --version
 ## 检查软链接指向
-ll /usr/bin/{python,python3,python$MAJOR.$MINOR,pip,pip3,pip$MAJOR.$MINOR}
-ll /usr/bin/{python*,pip*}
+# ls -l /usr/bin/{python*,pip*}
+# ls -l /usr/bin/{python3.*,pip3.*}
+ls -l /usr/bin/{python,python3,python3.*,pip,pip3,pip3.*}
+## 检查版本
+# python --version
+# pip --version
+# python3 --version
+# pip3 --version
