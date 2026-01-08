8ef3153d57c822b670d55e7c5cb6f12c10c950be
leion <leion@mail.com>
2026-01-08 18:15:51 +0800

    feat(PowerShell): 添加PowerShell配置文件并设置oh-my-posh主题和代理
    初始化PowerShell配置文件，包含随机选择oh-my-posh主题和设置代理环境变量

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-# PowerShell 启动时随机选择 oh-my-posh 主题并初始化
+# oh-my-posh设置：PowerShell 启动时随机选择主题并初始化
 # 脚本位置：~/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
 # 主题下载地址：https://github.com/JanDeDobbeleer/oh-my-posh/releases
 # 主题目录：~/Documents/WindowsPowerShell/ohmyposh/themes (需要手动创建)
@@ -9,3 +9,12 @@ $theme = $themes | Get-Random
 $themeFullPath = Join-Path -Path $themesPath -ChildPath $theme
 
 oh-my-posh init pwsh --config $themeFullPath | Invoke-Expression
+
+
+
+# 代理配置：
+$env:HTTP_PROXY = "http://127.0.0.1:7890"
+$env:HTTPS_PROXY = "http://127.0.0.1:7890"
+$env:ALL_PROXY = "http://127.0.0.1:7890"
+
+
