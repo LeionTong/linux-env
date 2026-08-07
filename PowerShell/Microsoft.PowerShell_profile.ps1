d9015edaf89d35822de8c4c0da1692e0b7fc5bb4
leion <leion@mail.com>
2026-08-07 09:59:42 +0800

    by .autocip.

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,20 +1,18 @@
+#----------------------------------------------------------#
 # oh-my-posh设置：PowerShell 启动时随机选择主题并初始化
-# 脚本位置：~/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+# 脚本位置：~/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
 # 主题下载地址：https://github.com/JanDeDobbeleer/oh-my-posh/releases
-# 主题目录：~/Documents/WindowsPowerShell/ohmyposh/themes (需要手动创建)
-
-$themesPath = "~/Documents/WindowsPowerShell/ohmyposh/themes"
+# 主题目录：~/Documents/PowerShell/ohmyposh/themes (需要手动创建)
+$themesPath = "~/Documents/PowerShell/oh-my-posh/themes"
 $themes = Get-ChildItem -Path $themesPath -Filter "*.omp.*" | Select-Object -ExpandProperty Name
 $theme = $themes | Get-Random
 $themeFullPath = Join-Path -Path $themesPath -ChildPath $theme
-
 oh-my-posh init pwsh --config $themeFullPath | Invoke-Expression
 
-
-
+#----------------------------------------------------------#
 # 代理配置：
 $env:HTTP_PROXY = "http://127.0.0.1:7890"
 $env:HTTPS_PROXY = "http://127.0.0.1:7890"
 $env:ALL_PROXY = "http://127.0.0.1:7890"
 
-
+#----------------------------------------------------------#
